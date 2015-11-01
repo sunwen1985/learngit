067a54da23657aaec3f9dca67eadc95c0a3b11c0
sunwen <root@localhost.localdomain>
2015-11-01 04:52:01 -0800

    modified Hello.java

--- Hello.java
+++ Hello.java
@@ -2,8 +2,9 @@ public class Hello{
 
    public static void main(String []args){
 
-
+        System.out.println("Happy New Year!");
 	System.out.println("Hello World!");
+	System.out.println("Happy MySelf!");
     }
 
 }
